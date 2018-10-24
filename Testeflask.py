b02aed2629f6ef983b3cd78b9c9c6639267551b7
ymendes <44381190+ymendes@users.noreply.github.com>
2018-10-23 22:34:46 -0300

    Add files via upload

--- Testeflask.py
+++ Testeflask.py
@@ -1,6 +1,18 @@
-from flask import Flask, request, render_template
+# coding: utf-8
+
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from contextlib import closing
+
+# configuração
+DATABASE = '/tmp/Testeflask.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
 pessoa ={}
 pessoa["Nome"] = "Ana Mendes Sobrinho"
@@ -35,6 +47,23 @@ data={}
 data["pessoas"] = pessoas
 data["tipo"] = "Doutorado"
 
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def criar_bd():
+    with closing(conectar_bd()) as bd:
+        with app.open_resource('esquemabanco.sql',mode='r') as sql:
+            bd.cursor().executescript(sql.read())
+        bd.commit()
+criar_bd()
+
+@app.before_request
+def pre_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def encerrar_requisicao(exception):
+    g.bd.close()
 
 @app.route("/listpessoa")
 def listpessoa():
